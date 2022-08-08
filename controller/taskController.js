77c8458f7631bb47c0d0bf7392ef1cba1197be08
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2022-08-08 12:34:47 +0800

    add edit button

--- controller/taskController.js
+++ controller/taskController.js
@@ -2,6 +2,8 @@ const { format } = require('date-fns');
 const Task = require('../model/Task');
 
 const task_index = (req, res) => {
+    console.log(`req.query.id=${req.query.id}`)
+    //console.log(`req.body.id=${req.body.id}`)
     Task.find({ taname: req.session.username })
         .then(data => {
             res.render('task/index', {
@@ -38,8 +40,21 @@ const task_delete = (req, res) => {
     } )
 }
 
+const task_update = (req, res) => {
+    console.log(`id=${req.params.id}`)
+    // Task.deleteOne({ _id: req.params.id } )
+    // .then((data) => { 
+    //     console.log(data) 
+    //     res.redirect('/task')
+    // } )
+    // .catch((err) => { 
+    //     console.log(err)
+    // } )
+}
+
 module.exports = {
     task_index, 
     task_create,
-    task_delete
+    task_delete,
+    task_update
   }
--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { task_index, task_create, task_delete } = require('../controller/taskController')
+const { task_index, task_create, task_delete, task_update } = require('../controller/taskController')
 const auth = require('../middleware/auth')
 
-router.get('/', auth, (req, res) => task_index(req, res))
+router.get('/', (req, res) => task_index(req, res))
 
 router.post('/new', (req, res) => task_create(req, res))
 
 router.delete('/delete/:id', (req, res) => task_delete(req, res))
+
+router.put('/put/:id', (req, res) => task_update(req, res))
 
 router.get('/:id', (req, res) => {
     console.log(req.params.id)
