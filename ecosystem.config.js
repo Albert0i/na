bd76caa8c3b44746be96445502473fff0df4d086
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2022-08-12 11:04:25 +0800

    add pm2 deploy function

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,11 +8,13 @@ module.exports = {
     }],
     deploy : {
       production : {
-        user : 'SSH_USERNAME',
-        host : 'SSH_HOSTMACHINE',
-        ref  : 'origin/master',
-        repo : 'GIT_REPOSITORY',
-        path : 'DESTINATION_PATH',
+        key: "../ssh-key-2022-05-09(2).pem",
+        user : 'ubuntu',
+        host : '140.238.40.147',
+        ssh_options: "StrictHostKeyChecking=no",
+        ref  : 'main',
+        repo : 'https://github.com/Albert0i/na.git',
+        path : '/home/ubuntu/na',
         'pre-deploy-local': '',
         'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
         'pre-setup': ''
@@ -20,9 +22,18 @@ module.exports = {
     }
   };
 /*
+  pm2 deploy production setup
+
+  pm2 deploy production
+
+  pm2 deploy production --force
+
   Scaling Node.js Applications With PM2 Clusters
   https://medium.com/geekculture/scaling-node-js-applicationswith-pm2-clusters-c216c4468d66
 
   LOAD-BALANCING | GUIDE | PM2 DOCUMENTATION
   https://pm2.io/docs/runtime/guide/load-balancing/
+
+  EASY DEPLOY WITH SSH
+  https://pm2.io/docs/runtime/guide/easy-deploy-with-ssh/
 */    
