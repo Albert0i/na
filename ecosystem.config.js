f4ccb87ecab763ea20678ee98c2dbd9a2b1d9f52
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2022-08-12 11:35:01 +0800

    minir

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
         user : 'ubuntu',
         host : '140.238.40.147',
         ssh_options: "StrictHostKeyChecking=no",
-        ref  : 'main',
+        ref  : 'origin/main',
         repo : 'https://github.com/Albert0i/na.git',
         path : '/home/ubuntu/na',
         'pre-deploy-local': '',
